71e0924aae4eda84464efcbd889b02cc172cffe9
Aaron-Gartner <gart1353@stthomas.edu>
2021-02-11 19:22:13 -0600

    finished

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,9 @@
 // compile: g++ -std=c++11 -o pointers pointers.cpp
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <climits>
+
 
 typedef struct Student {
     int id;
@@ -14,16 +17,37 @@ int promptInt(std::string message, int min, int max);
 double promptDouble(std::string message, double min, double max);
 void calculateStudentAverage(void *object, double *avg);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     Student student;
     double average;
-
+    int isValid = 0;
+    student.f_name = new char[128];
+    student.l_name = new char[128];
     // Sequence of user input -> store in fields of `student`
-
-    // Call `CalculateStudentAverage(???, ???)`
+    //ID number
+    student.id = promptInt("Please enter the student's ID number: ", 1, 999999999);
+    //First name
+    std::cout << "Please enter the student's FIRST name: ";
+    scanf("%s", student.f_name);
+    //Last name
+    std::cout << "Please enter the student's LAST name: ";
+    scanf("%s", student.l_name);
+    //Number of assignments
+    student.n_assignments = promptInt("Please enter how many assignments were graded: ", 1, 134217728);
+    student.grades = new double[student.n_assignments];
+    //Grades
+    for (int i = 0; i < student.n_assignments; i++) {
+        std::string message = "Please enter grade for assignment " + std::to_string(i) + ": ";
+        student.grades[i] = promptDouble(message, 0.0, 1000.0);
+    }
+    // Calculates average
+    double zero = 0.0;
+    double *avg = &zero; 
+    calculateStudentAverage(&student, avg);
     // Output `average`
-
+    printf("Student: %s %s [%d] \n", student.f_name, student.l_name, student.id);
+    //check tab or two spaces
+    printf("  Average Grade: %.1lf \n", *avg);
     return 0;
 }
 
@@ -32,9 +56,18 @@ int main(int argc, char **argv)
    min: minimum value to accept as a valid int
    max: maximum value to accept as a valid int
 */
-int promptInt(std::string message, int min, int max)
-{
+int promptInt(std::string message, int min, int max) {
     // Code to prompt user for an int
+    int input;
+    std::cout << message;
+    while (!(std::cin >> input) || input < min || input > max) {
+        std::cout << "Sorry, I cannot understand your answer";
+        std::cout << "\n";
+        std::cin.clear();
+        std::cin.ignore(INT_MAX, '\n');    
+        std::cout << message;
+    }
+    return input;
 }
 
 /*
@@ -42,16 +75,80 @@ int promptInt(std::string message, int min, int max)
    min: minimum value to accept as a valid double
    max: maximum value to accept as a valid double
 */
-double promptDouble(std::string message, double min, double max)
-{
-    // Code to prompt user for a double
+double promptDouble(std::string message, double min, double max) {
+    double input;
+    std::cout << message;
+    while (!(std::cin >> input) || input < min || input > max) {
+        std::cout << "Sorry, I cannot understand your answer";
+        std::cout << "\n";
+        std::cin.clear();
+        std::cin.ignore(INT_MAX, '\n');    
+        std::cout << message;
+    }
+    return input;
 }
 
 /*
    object: pointer to anything - your choice! (but choose something that will be helpful)
    avg: pointer to a double (can store a value here)
 */
-void calculateStudentAverage(void *object, double *avg)
-{
+void calculateStudentAverage(void *object, double *avg) {
     // Code to calculate and store average grade
+    Student *student = (Student*)object; 
+    int size = student->n_assignments;
+    //for number of objects add them all together and then divide by the number
+    for (int i = 0; i < size; i++) {
+        *avg += student->grades[i]; 
+    }
+    *avg = *avg / (double)size;
 }
+
+/*
+while (isValid == 0) {
+        if (isValid == 1) {
+            return doubleInput;
+        } else { 
+                //begin checking input
+                while (checked != 1) {
+                    //checking for empty input 
+                    if (stringInput.size() > 0) {
+                        //if string not empty ensure entry is valid numbers
+                        int i = 0;
+                        while(i < stringInput.size()) {
+                            if (isdigit(stringInput[i]) == false && stringInput[i] != '.') {
+                                i = 0;
+                                printf("%s\n", "char here");
+                                std::cout << "Sorry, I cannot understand your answer";
+                                std::cout << "\n";
+                                std::cout << message;
+                                std::cin >> stringInput; 
+                                checked = -1;
+                            }
+                            i++;
+                        } 
+                        if (checked != -1) {
+                            printf("in here\n");
+                            checked = 1;
+                        }
+                    } else {
+                        std::cout << "Sorry, I cannot understand your answer";
+                        std::cout << "\n";
+                        std::cout << message;
+                        std::cin >> stringInput;
+                        checked = 0;
+                    }
+                }
+                //input has been checked for no numerical now check for min/max issues
+                printf("%s\n", "double here");
+                doubleInput = std::stod(stringInput); 
+                if (doubleInput < min || doubleInput >= max) {
+                    std::cout << "Sorry, I cannot understand your answer";
+                    std::cout << "\n";
+                    std::cout << message;
+                    std::cin >> stringInput;
+                } else {
+                    isValid = 1;
+                }
+            }
+        }*/
+
